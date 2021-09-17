8f4554b8ece1ba230edd3ca559fbdad659f89037
Kristina Robinson <krobinson@martingroupmarketing.com>
2021-09-17 13:48:18 -0400

    Updates who plays or queues a song

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-const Discord = require("discord.js");
-// const {
-//   prefix, token
-// } = require("./config.json");
-const prefix = "-";
-const token = process.env['COSMIC_BOT_TOKEN'];
-const ytdl = require("ytdl-core");
+// core & third-party modules
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
 const search = require('yt-search');
 
-const client = new Discord.Client();
+// token and prefix
+const {
+  prefix, token
+} = require('./config.json');
+// const prefix = '-';
+// const token = process.env['COSMIC_BOT_TOKEN'];
 
+const client = new Discord.Client();
 const queue = new Map();
 
-client.once("ready", () => {
-  console.log("Ready ✨");
+// const commands = require('./commands/*');
+
+client.once('ready', () => {
+  console.log('Ready ✨');
 });
 
-client.once("reconnecting", () => {
-  console.log("Reconnecting!");
+client.once('reconnecting', () => {
+  console.log('Reconnecting!');
 });
 
-client.once("disconnect", () => {
-  console.log("Disconnect!");
+client.once('disconnect', () => {
+  console.log('Disconnect!');
 });
 
-client.on("message", async message => {
+client.on('voiceStateUpdate', (oldState, newState) => {
+  // check if someone connects or disconnects
+  if (oldState.channelID === null || typeof oldState.channelID == 'undefined') return;
+  // check if the bot is disconnecting
+  if (newState.id !== client.user.id) return;
+  // clear the queue
+  return queue.delete(oldState.guild.id);
+});
+
+client.on('message', async message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
 
@@ -61,7 +74,7 @@ client.on("message", async message => {
       .setTitle('Commands List')
       .setColor('#D09CFF')
       .setThumbnail('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/be21784c-ba2a-4df4-bdd8-b5568ea11ec8/dbjo53q-4683aad4-5549-4d28-ab4c-64f4bfc6a309.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2JlMjE3ODRjLWJhMmEtNGRmNC1iZGQ4LWI1NTY4ZWExMWVjOFwvZGJqbzUzcS00NjgzYWFkNC01NTQ5LTRkMjgtYWI0Yy02NGY0YmZjNmEzMDkuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.8s-9vXfdIbwONFVQQ3wMsIeJfFRdkiPwbr2d-jk2tt8')
-      .setDescription('\n**-brownies** : you\'re asking for it\n**-decosmic** : disconnects the bot\n**-dripless** : displays a dripless beetch\n**-guildmaster** : pleathhh\n**-help** : read this shit again\n**-p** : plays a song from YouTube\n**-play** : plays a song from YouTube\n**-pineapple** : displays a pineapple being eaten\n**-skip** : skips a song in queue\n**-next** : skips a song in queue\n**-stop** : stops the bot and clears the queue\n**-yeyur** : displays a man drunk on a toilet\n');
+      .setDescription('\n**Music Commands** \n**-p**: plays a song from YouTube\n**-play**: plays a song from YouTube\n**-skip**: skips a song in queue\n**-next**: skips a song in queue\n**-stop**: stops the bot and clears the queue\n\n**-decosmic**: disconnects the bot\n**-help**: read this shit again\n\n**Misc Commands**\n**-brownies**: you\'re asking for it\n** -dripless **: displays a dripless beetch\n**-guildmaster**: pleathhh\n**-pineapple **: displays a pineapple being eaten\n**-yeyur** : displays a man drunk on a toilet\n');
     message.channel.send(commandsEmbed);
   } else if (message.content.startsWith(`${prefix}pineapple`)) {
     const pineappleEmbedFile = new Discord.MessageAttachment('./img/pineapple.jpg');
@@ -85,33 +98,27 @@ client.on("message", async message => {
       .setColor('#1BCCE8');
     message.channel.send(yeyur);
   } else {
-    message.channel.send("I dunno what that means.\nNeed help? Type **-help**");
+    message.channel.send('I dunno what that means.\nNeed help? Type **-help**');
   }
-});
-
-client.on('voiceStateUpdate', (oldState, newState) => {
-  // check if someone connects or disconnects
-  if (oldState.channelID === null || typeof oldState.channelID == 'undefined') return;
-  // check if the bot is disconnecting
-  if (newState.id !== client.user.id) return;
-  // clear the queue
-  return queue.delete(oldState.guild.id);
 });
 
 async function execute(message, serverQueue) {
-  const args = message.content.split(" ");
+  const args = message.content.split(' ');
+  // TODO: checks if args is present before we can replace
+  // const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
   const voiceChannel = message.member.voice.channel;
   const permissions = voiceChannel.permissionsFor(message.client.user);
+  const author = message.member.displayName;
 
   if (!voiceChannel) {
     return message.channel.send(
-      "You need to be in a voice channel to play music!"
+      'You need to be in a voice channel to play music!'
     );
   }
 
-  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+  if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
     return message.channel.send(
-      "I need the permissions to join and speak in your voice channel!"
+      'I need the permissions to join and speak in your voice channel!'
     );
   }
 
@@ -123,7 +130,7 @@ async function execute(message, serverQueue) {
       title: songInfo.videoDetails.title,
       url: songInfo.videoDetails.video_url,
     };
-  } else { // otherwise find a vid and play the first result
+  } else { // otherwise search YT and play the first result
     const video_finder = async(query)=> {
       const videoResult = await search(query);
       return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
@@ -134,13 +141,13 @@ async function execute(message, serverQueue) {
         title: video.title,
         url: video.url,
       };
-    } else {
+    } else { // runs a check to see if we're supposed to leave the server
       if (message.content === '-decosmic') {
         const decosmic = new Discord.MessageEmbed()
           .setDescription(`👋🏼 Baiii `)
           .setColor('#D09CFF');
         return message.channel.send(decosmic);
-      } else {
+      } else { // we couldn't find a song
         const noVid = new Discord.MessageEmbed()
           .setDescription(`Sorry, I couldn't find anything to play!`)
           .setColor('#D09CFF');
@@ -163,7 +170,7 @@ async function execute(message, serverQueue) {
     try {
       var connection = await voiceChannel.join();
       queueContruct.connection = connection;
-      play(message.guild, queueContruct.songs[0]);
+      play(author, message.guild, queueContruct.songs[0]);
     } catch (err) {
       console.log(err);
       queue.delete(message.guild.id);
@@ -173,10 +180,36 @@ async function execute(message, serverQueue) {
     const queueing = new Discord.MessageEmbed()
       .setTitle(`📌 Queuein' up`)
       .setColor('#4FDFED')
-      .setDescription(`${song.title}`);
+      .setDescription(`${song.title}`)
+      .setFooter(`Added by ${author}`);
     serverQueue.songs.push(song);
     return message.channel.send(queueing );
   }
+}
+
+function play(author, guild, song) {
+  const serverQueue = queue.get(guild.id);
+  if (!song) {
+    queue.delete(guild.id);
+    return;
+  }
+
+  const dispatcher = serverQueue.connection
+    .play(ytdl(song.url, {
+      filter: 'audioonly'
+    }))
+    .on('finish', () => {
+      serverQueue.songs.shift();
+      play(guild, serverQueue.songs[0]);
+    })
+    .on('error', error => console.error(error));
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  const playing = new Discord.MessageEmbed()
+    .setTitle(`🎶 Now Playing 🎶 `)
+    .setColor('#79E676')
+    .setDescription(`${song.title}`)
+    .setFooter(`Added by ${author}`);
+  serverQueue.textChannel.send(playing);
 }
 
 function skip(message, serverQueue) {
@@ -220,28 +253,6 @@ function decosmic(message) {
     .setColor('#D09CFF');
   message.guild.me.voice.channel.leave();
   return message.channel.send(decosmic);
-}
-
-function play(guild, song) {
-  const serverQueue = queue.get(guild.id);
-  if (!song) {
-    queue.delete(guild.id);
-    return; 
-  }
-
-  const dispatcher = serverQueue.connection
-    .play(ytdl(song.url, { filter: 'audioonly' }))
-    .on("finish", () => {
-      serverQueue.songs.shift();
-      play(guild, serverQueue.songs[0]);
-    })
-    .on("error", error => console.error(error));
-  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-  const playing = new Discord.MessageEmbed()
-    .setTitle(`🎶 Now Playing 🎶 `)
-    .setColor('#79E676')
-    .setDescription(`${song.title}`);
-  serverQueue.textChannel.send(playing);
 }
 
 client.login(token);
